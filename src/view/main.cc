26487f0ede821003c87234a6378b66ff50fcec81
mortylar <mortylar@student.21-school.ru>
2023-12-17 16:47:40 +0700

    dfgse

--- src/view/main.cc
+++ src/view/main.cc
@@ -25,14 +25,14 @@
   pannel.SetNames(1, "first");
   pannel.SetValue("Second");
   pannel.SetName("123");
-  gtk_widget_show (window);
+  gtk_widget_set_visible(window, true);
 }
 
 int main (int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK(Activate), NULL);
   status = g_application_run (G_APPLICATION(app), argc, argv);
   g_object_unref (app);
