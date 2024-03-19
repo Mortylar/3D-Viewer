e515c6b3949efca64e762d65e8fe1032c0c5d08e
mortylar <mortylar@student.21-school.ru>
2024-03-19 23:20:53 +0700

    Update figure - add textures and normals date

--- src/model/figure.cc
+++ src/model/figure.cc
@@ -13,7 +13,12 @@ s21::FigureKiller::~FigureKiller() {
 
 void s21::Figure::Clear() {
   vertex_->clear();
-  surfaces_->clear();
+  textures_->clear();
+  normals_->clear();
+
+  v_surfaces_->clear();
+  t_surfaces_->clear();
+  n_surfaces_->clear();
 }
 
 //---------------------------vertexes--------------------------//
@@ -26,6 +31,15 @@ std::vector<float>& s21::Figure::GetVertex() {
   return *vertex_;
 }
 
+std::vector<float>& s21::Figure::GetTextures() {
+  return *textures_;
+}
+
+std::vector<float>& s21::Figure::GetNormals() {
+  return *normals_;
+}
+
+
 //const float* s21::Figure::GetVertexData() {
 //  return vertex_->data();
 //}
@@ -36,6 +50,18 @@ void s21::Figure::AddVertex(float x, float y, float z) {
   vertex_->push_back(z);
 }
 
+void s21::Figure::AddTextures(float x, float y, float z) {
+  textures_->push_back(x);
+  textures_->push_back(y);
+  textures_->push_back(z);
+}
+
+void s21::Figure::AddNormals(float x, float y, float z) {
+  normals_->push_back(x);
+  normals_->push_back(y);
+  normals_->push_back(z);
+}
+
 
 //-------------------------surfaces----------------------------//
 
@@ -43,11 +69,27 @@ size_t s21::Figure::GetSurfacesCount() {
   return surfaces_->size();
 }
 
-const std::vector<unsigned int>& s21::Figure::GetSurface(size_t i) {
-  return surfaces_->at(i);
+const std::vector<unsigned int>& s21::Figure::GetVSurface(size_t i) {
+  return v_surfaces_->at(i);
 }
 
-void s21::Figure::AddSurface(const std::vector<unsigned int>& surface) {
-  surfaces_->push_back(surface);
+const std::vector<unsigned int>& s21::Figure::GetTSurface(size_t i) {
+  return t_surfaces_->at(i);
 }
 
+const std::vector<unsigned int>& s21::Figure::GetNSurface(size_t i) {
+  return n_surfaces_->at(i);
+}
+
+
+void s21::Figure::AddVSurface(const std::vector<unsigned int>& surface) {
+  v_surfaces_->push_back(surface);
+}
+
+void s21::Figure::AddTSurface(const std::vector<unsigned int>& surface) {
+  t_surfaces_->push_back(surface);
+}
+
+void s21::Figure::AddNSurface(const std::vector<unsigned int>& surface) {
+  n_surfaces_->push_back(surface);
+}
--- src/model/figure.h
+++ src/model/figure.h
@@ -34,12 +34,24 @@ class Figure {
     }
 
     size_t GetVertexCount();
+    
     std::vector<float>& GetVertex();
-    void AddVertex(float x, float y, float z);
+    std::vector<float>& GetTextures();
+    std::vector<float>& GetNormals();
+
+    void AddVertex(float x, float y, float z); 
+    void AddTextures(float x, float y, float z);
+    void AddNormals(float x, float y, float z);
 
     size_t GetSurfacesCount();
-    const std::vector<unsigned int>& GetSurface(size_t n);
-    void AddSurface(const std::vector<unsigned int>& x);
+
+    const std::vector<unsigned int>& GetVSurface(size_t n);
+    const std::vector<unsigned int>& GetTSurface(size_t n);
+    const std::vector<unsigned int>& GetNSurface(size_t n);
+
+    void AddVSurface(const std::vector<unsigned int>& x);
+    void AddTSurface(const std::vector<unsigned int>& x);
+    void AddNSurface(const std::vector<unsigned int>& x);
 
     void Clear();
 
@@ -47,17 +59,28 @@ class Figure {
     static Figure* self_;
     static FigureKiller killer_;
     std::vector<float>* vertex_;
-    std::vector<std::vector<unsigned int>>* surfaces_;
+    std::vector<float>* textures_;
+    std::vector<float>* normals_;
+    std::vector<std::vector<unsigned int>>* v_surfaces_; 
+    std::vector<std::vector<unsigned int>>* t_surfaces_;
+    std::vector<std::vector<unsigned int>>* n_surfaces_;
 
     Figure() {
       vertex_ = new std::vector<float>();
-      surfaces_ = new std::vector<std::vector<unsigned int>>();
+      textures_ = new std::vector<float>();
+      normals_ = new std::vector<float>();
+      v_surfaces_ = new std::vector<std::vector<unsigned int>>(); 
+      t_surfaces_ = new std::vector<std::vector<unsigned int>>();
+      n_surfaces_ = new std::vector<std::vector<unsigned int>>();
     }
 
     ~Figure() {
       delete vertex_;
-      delete surfaces_;
-      std::cout << "\n~Figure()\n" << std::endl;
+      delete textures_;
+      delete normals_;
+      delete v_surfaces_;
+      delete t_surfaces_;
+      delete n_surfaces_;
     }
 
     Figure(const Figure&) = delete;
