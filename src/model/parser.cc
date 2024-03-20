937ea148bbcf8ab8e88abd70bc06851d7db55668
mortylar <mortylar@student.21-school.ru>
2024-03-21 02:30:00 +0700

    Update parser

--- src/model/parser.cc
+++ src/model/parser.cc
@@ -1,79 +1,168 @@
 #include "parser.h"
 
+#include <cstring>
 #include <fstream>
 #include <stdexcept>
 #include <vector>
-#include <cstring>
 
 #include "figure.h"
 
 #include <iostream> //TODO remove
 
-void s21::Parser::ParserMethod(const char* file_name) {
+void s21::Parser::ParserMethod(const char *file_name) {
   setlocale(LC_NUMERIC, "C");
-  std::ifstream fin(file_name);
-  if (!fin.is_open())
-    throw std::runtime_error("s21::Parser::ParserMethod(const char*) - couldn't open the file");
+  size_t file_length = GetFileLength(file_name);
+  FILE *fp = fopen(file_name, "rb");
+  if (!fp)
+    throw std::runtime_error(
+        "s21::Parser::ParserMethod(const char*) - couldn't open the file");
 
   ResetFigure();
-  char buffer[buffer_length_]{0};
-  fin >> buffer;
-  while (buffer[0]) {
-    if (('v' == buffer[0]) && ('\0' == buffer[1])) {  
-      ReadVertexes(fin);
-    } else if (('f' == buffer[0]) && ('\0' == buffer[1])) {
-      ReadSurface(fin);
+  while (file_length) {
+    std::string buffer = ReadString(fp);
+    if ('v' == buffer[0]) {
+      ReadVertexes(buffer);
+    } else if (('f' == buffer[0]) && (' ' == buffer[1])) {
+      ReadSurface(buffer);
     }
-    buffer[0] = 0;
-    fin >> buffer;
   }
 
-  fin.close();
-	s21::Figure::GetInstance()->GetVertex().shrink_to_fit();
+  fclose(fp);
+  s21::Figure::GetInstance()->GetVertex().shrink_to_fit();
   if (!s21::Figure::GetInstance()->GetVertexCount())
     throw std::invalid_argument("Empty figure");
-
 }
 
-void s21::Parser::ReadVertexes(std::ifstream& fin) {
-  char buffer[buffer_length_]{0};
+void s21::Parser::ReadVertexes(std::string buffer) {
   float vertex[3]{0};
-  for (int i = 0; i < 3; ++i) {
-    fin >> buffer;
-    if ((!IsNumber(buffer[0])) || (fin.eof()))
-      throw std::invalid_argument("s21::Parser - object file include less " 
-                 "then 3 coordinates in one vertex");
-    vertex[i] = std::strtod(buffer, nullptr);
+  int status = 0;
+
+  if (buffer[1] == ' ') {
+    status = ExtractVertexData(buffer, "v %f %f %f", vertex[0], vertex[1],
+                               vertex[2]);
+    s21::Figure::GetInstance()->AddVertex(vertex[0], vertex[1], vertex[2]);
+  } else if (buffer[1] == 't') {
+    status = ExtractVertexData(buffer, "vt %f %f %f", vertex[0], vertex[1],
+                               vertex[2]);
+    s21::Figure::GetInstance()->AddTextures(vertex[0], vertex[1], vertex[2]);
+  } else if (buffer[1] == 'n') {
+    status = ExtractVertexData(buffer, "vn %f %f %f", vertex[0], vertex[1],
+                               vertex[2]);
+    s21::Figure::GetInstance()->AddNormals(vertex[0], vertex[1], vertex[2]);
   }
-  s21::Figure::GetInstance()->AddVertex(vertex[0], vertex[1], vertex[2]);
+  if (status != 3)
+    throw std::invalid_argument("s21::Parser - object file include less then 3 "
+                                "coordinates in one vertex");
 }
 
-void s21::Parser::ReadSurface(std::ifstream& fin) {
-  char buffer[buffer_length_]{0};
-  std::vector<unsigned int> surface;
-  int v_count = static_cast<int>(s21::Figure::GetInstance()->GetVertexCount());
-  fin >> buffer;
-  while(IsNumber(buffer[0])) {
-    int vertex_id = std::atoi(buffer);
-    if (vertex_id < 0) {
-      vertex_id = v_count + 1 + vertex_id;
-    }
-    if ((vertex_id <= 0) || (vertex_id > v_count))
-      throw std::invalid_argument("s21::Parser - invalid number of vertex");
+int s21::Parser::ExtractVertexData(std::string &buffer, std::string format,
+                                   float &x, float &y, float &z) {
+  return sscanf(buffer.data(), format.data(), &x, &y, &z);
+}
 
-    surface.push_back(static_cast<unsigned int>(vertex_id - 1));
-    //surface[vertex_id] = surface[vertex_id]; //+ 1; //TODO
-    fin >> buffer;
+void s21::Parser::ReadSurface(const std::string &buffer) {
+  std::vector<unsigned int> v_surface;
+  std::vector<unsigned int> t_surface;
+  std::vector<unsugned int> n_surface;
+  size_t pos = 2;
+  while (pos < buffer.size()) {
+    pos = ExtractFragment(buffer, pos, v_surface, t_surface, n_surface);
+    while (!IsNumber(buffer[pos])) {
+      ++pos;
+    }
   }
-  surface.shrink_to_fit();
-  s21::Figure::GetInstance()->AddSurface(static_cast<const std::vector<unsigned int>>(surface));
-  fin.seekg(fin.tellg() - static_cast<std::streamoff>(strlen(buffer) + 1));
+
+  s21::Figure::GetInstance()->AddVSurface(v_surface);
+  if (t_surface.size())
+    s21::Figure::GetInstance()->AddTSurface(t_surface);
+  if (n_surface.size())
+    s21::Figure::GetInstance()->AddNSurface(n_surface);
+}
+
+size_t s21::Parser::ExtractFragment(const std::string &buffer, size_t pos,
+                                    std::vector<unsigned int> &v,
+                                    std::vector<unsigned int> &t,
+                                    std::vector<unsigned int> &n) {
+  std::string fragment;
+  int mod_count = 0;
+
+  while (buffer[pos] && (buffer[pos] != ' ') && (buffer[pos] != '\n')) {
+    if (buffer[pos] == '/') {
+      fragment += ' ';
+      ++mod_count;
+      if (buffer[pos + 1] == '/') {
+        fragment += "0 ";
+        pos += 2;
+        ++mod_count;
+      } else {
+        fragment += ' ';
+        ++pos;
+      }
+    } else {
+      fragment += buffer[pos];
+      ++pos;
+    }
+  }
+  WriteFragment(fragment, mod_count, v, t, n);
+  return pos;
+}
+
+void s21::Parser::WriteFragment(const std::string &fragment, int mod_count,
+                                std::vector<unsigned int> &v,
+                                std::vector<unsigned int> &t,
+                                std::vector<unsigned int> &n) {
+  int vertex_id, texture_id, normal_id;
+  int status =
+      sscanf(fragment.data(), "%i %i %i", &vertex_id, &texture_id, &normal_id);
+  if (status == 0)
+    throw std::invalid_argument("s21::Parser - empty surface");
+
+  int vertex_count =
+      static_cast<int>(s21::Figure::GetInstance()->GetVertexCount());
+  if (vertex_id < 0) {
+    vertex_id = vertex_count + vertex_id + 1;
+  }
+  if ((vertex_id <= 0) || (vertex_id > vertex_count))
+    throw std::invalid_argument("s21::Parser - invalid number of vertex");
+  v.push_back(static_cast<unsigned int>(vertex_id - 1));
+
+  if (status == 3) {
+    t.push_back(static_cast<unsigned int>(texture_id));
+    n.push_back(static_cast<unsigned int>(normal_id));
+  } else if (status == 2) {
+    if (mod_count == 2) {
+      n.push_back(static_cast<unsigned int>(normal_id));
+    } else {
+      t.push_back(static_cast<unsigned int>(texture_id));
+    }
+  }
 }
 
 bool s21::Parser::IsNumber(char ch) {
-  return std::isdigit(static_cast<unsigned char>(ch)) || ('+' == ch ) || ('-' == ch);
+  return std::isdigit(static_cast<unsigned char>(ch)) || ('+' == ch) ||
+         ('-' == ch);
 }
 
-void s21::Parser::ResetFigure() {
-  s21::Figure::GetInstance()->Clear();
+size_t s21::Parser::GetFileLength(const char *file_name) {
+  size_t file_length = 0;
+  FILE *fp = fopen(file_name, "rb");
+  if (fp) {
+    fseek(fp, 0, SEEK_END);
+    file_length = ftell(fp);
+    fclose(fp);
+  }
+  return file_length;
 }
+
+std::string s21::Parser::ReadString(FILE *fp, size_t *file_length) {
+  std::string result{0};
+  char letter{0};
+  while ((*file_length) && (letter != '\n') && letter) {
+    fread(&letter, 1, 1, fp);
+    result += letter;
+    --(*file_length);
+  }
+  return result;
+}
+
+void s21::Parser::ResetFigure() { s21::Figure::GetInstance()->Clear(); }
--- src/model/parser.h
+++ src/model/parser.h
@@ -5,22 +5,30 @@
 
 namespace s21 {
 class Parser {
-  public:
-    Parser() = default;
-    ~Parser() = default;
+public:
+  Parser() = default;
+  ~Parser() = default;
 
-    void ParserMethod(const char* file_name);
+  void ParserMethod(const char *file_name);
 
-  private:
-    const int buffer_length_ = 255;
-    
-    void ReadVertexes(std::ifstream& fin);
-    void ReadSurface(std::ifstream& fin);
-    void ResetFigure();
+private:
+  // const int buffer_length_ = 255;
 
-    bool IsNumber(char ch);
+  void ReadVertexes(std::string &buffer);
+  int ExtractVertexData(const std::string &, std::string, float &, float &,
+                        float &);
+
+  void ReadSurface(const std::string &buffer);
+  size_t ExtractFragment(const std::string &, size_t,
+                         std::vector<unsigned int> &std::vector<unsigned int>
+                             &std::vector<unsigned int> &);
+  void WriteFragment(const std::string &, int, std::vector<unsigned int> &,
+                     std::vector<unsigned int> &std::vector<unsigned int> &);
+  void ResetFigure();
+  size_t GetFileLength(const char *file_name);
+  std::string ReadString(FILE *, size_t *);
+  bool IsNumber(char ch);
 };
-}
-
+} // namespace s21
 
 #endif // SRC_MODEL_PARSER_H_
