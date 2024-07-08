2a0db82333626bf0135b23011d2fdeae35b94827
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-07-09 02:08:18 +0700

    Add cc file for opengl_model

--- src/model/opengl_model.h
+++ src/model/opengl_model.h
@@ -23,7 +23,7 @@
 
 #include "../libs/data.h"
 #include "affine_3d.h"
-#include "figure.h" //TODO try to remove
+//#include "figure.h" //TODO try to remove
 
 namespace s21 {
 
@@ -37,161 +37,41 @@ public:
   ~Buffer() { Clear(); }
 
   void CreateBuffer(std::vector<float> &v_data,
-                    std::vector<unsigned int> &e_data) {
-    Clear();
-    CreateVertexBuffer(v_data);
-    CreateElementBuffer(e_data);
-  }
-
-  GLuint &GetVertexBuffer() { return vertex_; }
-
-  GLuint &GetElementBuffer() { return element_; }
-
-
-  void Clear() {
-    if (vertex_)
-      glDeleteBuffers(1, &vertex_);
-    if (element_)
-      glDeleteBuffers(1, &element_);
-  }
+                    std::vector<unsigned int> &e_data);
+  inline GLuint &GetVertexBuffer() { return vertex_; }
+  inline GLuint &GetElementBuffer() { return element_; }
+  void Clear();
 
 private:
   GLuint vertex_ = 0;
   GLuint element_ = 0;
 
-  void CreateVertexBuffer(std::vector<float> &data) {
-    glGenBuffers(1, &vertex_);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * data.size(), data.data(),
-                 GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-  }
-
-  void CreateElementBuffer(std::vector<unsigned int> &data) {
-      glGenBuffers(1, &element_);
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_);
-      glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                   sizeof(unsigned int) * data.size(), data.data(),
-                   GL_STATIC_DRAW);
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-  }
-
+  void CreateVertexBuffer(std::vector<float> &data);
+  void CreateElementBuffer(std::vector<unsigned int> &data);
 };
 
 
 class Shader {
 public:
-  Shader() {g_print("\nshader start\n");};
+  Shader() {};
 
   ~Shader() { 
-//    glDeleteProgram(program_);
     program_ = 0;
   }
 
-  void LoadShader(const char* vertex_path, const char* fragment_path, const char* geometry_path) {
-    InitShader(vertex_path, fragment_path, geometry_path);
-  }
-
-  GLuint GetProgram() {
+  void LoadShader(const char* vertex_path, const char* fragment_path, const char* geometry_path);
+  inline GLuint GetProgram() {
     return program_;
   }
 
 private:
   GLuint program_ = 0;
 
-  void InitShader(const char* vertex_path, const char* fragment_path, const char* geometry_path) {
-    GLuint vertex, fragment, geometry;
-    int status;
-
-    vertex = CreateShader(GL_VERTEX_SHADER, LoadFile(vertex_path));
-
-    if (vertex) {
-      fragment = CreateShader(GL_FRAGMENT_SHADER, LoadFile(fragment_path));
-        if (fragment && geometry_path) {
-          g_print("\ngeom shader start %s\n", geometry_path);
-	  geometry = CreateShader(GL_GEOMETRY_SHADER, LoadFile(geometry_path));
-	  if (!geometry) {
-	    g_print("\ngeometry shader failed\n");
-	  }
-	}
-    } else {
-      g_print("\nvertex shader failed\n");
-      return; // TODO
-    }
-
-    if (!fragment) {
-      glDeleteShader(vertex);
-      g_print("\nfragment shader failed\n");
-      return;
-    }
-
-    program_ = glCreateProgram();
-    glAttachShader(program_, vertex);
-    glAttachShader(program_, fragment);
-    if (geometry_path) { 
-      glAttachShader(program_, geometry);
-      g_print("\nattach\n");
-    }
-    glLinkProgram(program_);
-
-    glGetProgramiv(program_, GL_LINK_STATUS, &status);
-    if (status == GL_FALSE) {
-      int log_len;
-      char* buffer;
-      glGetProgramiv(program_, GL_INFO_LOG_LENGTH, &log_len);
-      buffer = (char*)g_malloc(log_len + 1);
-      glGetProgramInfoLog(program_, log_len, NULL, buffer);
-      g_print("\nprogram link failed = %s\n", buffer);
-    } // TODO
-
-    glDetachShader(program_, vertex);
-    glDetachShader(program_, fragment);
-    glDetachShader(program_, geometry); //TODO
-
-    glDeleteShader(vertex);
-    glDeleteShader(fragment);
-    glDeleteShader(geometry);
-  }
-
-  GLuint CreateShader(int type, const char *src) {
-    GLuint shader;
-    int status;
-
-    shader = glCreateShader(type);
-    glShaderSource(shader, 1, &src, NULL);
-    glCompileShader(shader);
-
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-    if (status == GL_FALSE) {
-      int log_len;
-      char* buffer;
-      glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_len);
-      buffer = (char*)g_malloc(log_len + 1);
-      glGetShaderInfoLog(shader, log_len, NULL, buffer);
-      g_print("\ntype = %s\n", (type == GL_VERTEX_SHADER) ? "vertex" :
-                                   ((type == GL_FRAGMENT_SHADER) ? "fragment" : "geometry"));
-      g_print("\nprogram link failed = %s\n", buffer);
-
-    } // TODO
-    delete src;
-    return shader;
-  }
-
-  char *LoadFile(const char *file_name) {
-    int len = 4096; // TODO
-    char *text = new char[len]();
-    FILE *fp = fopen(file_name, "rb");
-    if (fp) {
-      fread(text, 1, len, fp);
-      fclose(fp);
-    } else {
-      g_print("\nError Opening File\n");
-    }
-    return text;
-  } //TODO GetFileLength
-
-
+  void InitShader(const char* vertex_path, const char* fragment_path, const char* geometry_path);
+	GLuint CreateShader(int type, const char* src);
+  char *LoadFile(const char *file_name);
 };
+
 
 class Texture {
 public:
@@ -199,25 +79,10 @@ public:
 
   ~Texture() {};
 
-  GLuint GetID() {
+  inline GLuint GetID() {
     return id_;
   }
-
-  void Create(const std::string& file_name) {
-    if (file_name != file_) {
-      file_ = file_name;
-      unsigned char* data = stbi_load(file_name.data(), &width_, &height_, &color_mod_, 0);
-      if (color_mod_ < 3) {} //TODO
-      glGenTextures(1, &id_);
-      glBindTexture(GL_TEXTURE_2D, id_);
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width_, height_, 0,
-                   ((color_mod_ == 3) ? GL_RGB : GL_RGBA), GL_UNSIGNED_BYTE, data);
-
-      SamplerInit();
-      glBindTexture(id_, 0);
-      stbi_image_free(data);
-    }
-  }
+  void Create(const std::string& file_name);
 
 private:
   GLuint id_ = 0;
@@ -226,12 +91,7 @@ private:
   int color_mod_ = 0;
   std::string file_;
 
-  void SamplerInit() {
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  }
+  void SamplerInit();
 };
 
 
@@ -251,74 +111,17 @@ public:
 //    DeleteProgram();
   }
 
-  void SetGLArea(GtkGLArea *area) { area_ = area; }
+  inline void SetGLArea(GtkGLArea *area) { area_ = area; }
 
-  void ConnectData(s21::Data *data) { data_ = data; }
+  inline void ConnectData(s21::Data *data) { data_ = data; }
 
-  void SetBuffer() {
-    gtk_gl_area_make_current(GTK_GL_AREA(area_));
-    InitBuffer();
-    InitShaders();
-    //std::string file = "3.jpg"; //TODO
-    //LoadTexture(file);
-  }
+  void SetBuffer();
+  void LoadTexture(std::string& file);
+  void Draw();
 
-  void LoadTexture(std::string& file) {
-    texture_image_.Create(file);
-  }//TODO
+  unsigned int* Mirroring(size_t width, size_t height, unsigned int* data);
 
-  void Draw() {
-    if (vertex_->GetVertexBuffer() != 0) {
-      DrawFigure();
-    }
-
-   // g_warning("\nepoxy = %i\n", epoxy_glsl_version());
-  }
-
-  unsigned int* Mirroring(size_t width, size_t height, unsigned int* data) {
-    unsigned int arr[width*height];
-    for (size_t i = 0; i < height; ++i) {
-      unsigned int* row = data + width*i;
-      unsigned int* arr_row = arr + width*(height - i - 1);
-      for (size_t j = 0; j < width; ++j) {
-        arr_row[j] = row[j];
-      }
-    }
-
-    for (size_t i = 0; i < width*height; ++i) {
-      data[i] = arr[i];
-    }
-    return data;
-  }
-
-  void SavePicture(size_t m_width, size_t m_height, const char* file_name, const char* type) {
-    g_print("\nsave picture\n");
-
-    size_t width = gtk_widget_get_width(GTK_WIDGET(area_));
-    size_t height = gtk_widget_get_height(GTK_WIDGET(area_));
-    size_t chanels = 4;
-
-	//	GLint default_frame_buffer; 
-	//	glGetIntegerv(GL_FRAMEBUFFER, &default_frame_buffer);
-	//	g_warning("\nframebuffer_id = %i\n", default_frame_buffer);
-
-    unsigned int* data = new unsigned int[width * height]{0};
-		//glBindBuffer(GL_FRAMEBUFFER, default_frame_buffer);
-    Draw();
-   // glPixelStorei(GL_PACK_ALIGNMENT, 1);
-    //glReadPixels(m_width - width, m_height - height, width, height, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, data);
-    
-    glReadPixels(m_width - width, m_height - height, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		data = Mirroring(width, height, data);
-   
-   stbi_write_png(file_name, width, height, chanels, data, chanels*width);
-   stbi_write_jpg(file_name, width, height, chanels, data, 80);
-   stbi_write_bmp(file_name, width, height, chanels, data);
-
-    g_print("\nw = %li\nh = %li\n", width, height);
-//    glBindFramebuffer(GL_FRAMEBUFFER, 0); 
-  }
-
+  void SavePicture(size_t m_width, size_t m_height, const char* file_name, const char* type);
 
 private:
   s21::Data *data_ = nullptr;
@@ -339,203 +142,21 @@ private:
   Buffer *texture_ = nullptr;
   Buffer *normals_ = nullptr;
 
-  void DrawFigure() {
-		g_warning("\nStart_draw\n");
-    Matrix4f mvp;
-    ComputeMVP(mvp);
-    ResetField();
+  void DrawFigure();
+  void ResetField();
+  void ConnectBuffers();
+  void DrawPoints(Matrix4f& mvp);
+  void DrawLines(Matrix4f& mvp);
 
-    ConnectBuffers();
+  void InitShaders();
+  void InitBuffer();
 
-    DrawLines(mvp);
-    DrawPoints(mvp);
-
-    glDisableVertexAttribArray(0);
-    glUseProgram(0);
-  }
-
-  void ResetField() {
-    GdkRGBA *color = data_->GetAreaColor();
-    glClearColor(color->red, color->green, color->blue, color->alpha + 1);
-    glEnable(GL_DEPTH_TEST);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //glClearDepth(1.0);
-  }
-
-  void ConnectBuffers() {
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_->GetVertexBuffer());
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                          nullptr);
-    glEnableVertexAttribArray(0);/*
-    glBindBuffer(GL_TEXTURE_BUFFER, texture_->GetVertexBuffer());
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                          nullptr); //TODO ->
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, normals_->GetVertexBuffer());
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                          nullptr); //TODO -> glTexCoordPointer
-    glEnableVertexAttribArray(2);*/
-  }
-
-  void DrawPoints(Matrix4f& mvp) {
-    if (data_->GetPointType()) {
-      mvp_location_ = glGetUniformLocation(point_shader.GetProgram(), "mvp");
-      color_location_ = glGetUniformLocation(point_shader.GetProgram(), "color");
-      glUseProgram(point_shader.GetProgram());
-      glBindVertexArray(vao_);
-      glUniformMatrix4fv(mvp_location_, 1, GL_TRUE, &mvp(0,0));
-      glEnableVertexAttribArray(0); //TODO??
-				    //
-      GdkRGBA color = *(data_->GetPointColor());
-      GLfloat point_color[4] {color.red, color.green, color.blue, color.alpha};
-      glUniform4fv(color_location_, 1, point_color);
-      glUniform1f(glGetUniformLocation(point_shader.GetProgram(), "size"), data_->GetPointSize());
-      glUniform1i(glGetUniformLocation(point_shader.GetProgram(), "type"), data_->GetPointType());
-      
-			glDrawArrays(GL_POINTS, 0, s21::Figure::GetInstance()->GetVertexCount());
-      glFlush();
-    }
-  }
-
-  void DrawLines(Matrix4f& mvp) {
-    if (data_->GetLineType()) {
-      mvp_location_ = glGetUniformLocation(line_shader.GetProgram(), "mvp");
-      //color_location_ = glGetUniformLocation(line_shader.GetProgram(), "color");
-      glUseProgram(line_shader.GetProgram());
-      glBindVertexArray(vao_);
-     // mvp.Print();
-      glUniformMatrix4fv(mvp_location_, 1, GL_TRUE, &mvp(0,0));
-      glEnableVertexAttribArray(0); //TODO??
-
-      GdkRGBA color = *(data_->GetLineColor());
-      GLfloat line_color[4] {color.red, color.green, color.blue, color.alpha};
-      glUniform4fv(glGetUniformLocation(line_shader.GetProgram(), "color"), 1, line_color);
-
-      color = *(data_->GetAreaColor());
-      GLfloat background_color[4] {color.red, color.green, color.blue, color.alpha};
-      glUniform4fv(glGetUniformLocation(line_shader.GetProgram(), "background_color"), 1, background_color);
-
-
-      glUniform1f(glGetUniformLocation(line_shader.GetProgram(), "size"), data_->GetLineWidth());
-      glUniform1i(glGetUniformLocation(line_shader.GetProgram(), "type"), data_->GetLineType());
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_->GetElementBuffer());
-
-      std::vector<unsigned int> offset = s21::Figure::GetInstance()->GetIndexOffset();
-
-      size_t last_position = 0;
-      glDrawElements(GL_LINE_LOOP, offset[0], GL_UNSIGNED_INT, (void *)(last_position));
-      for (size_t i = 1; i < offset.size(); ++i) {
-        last_position += offset[i-1] * sizeof(unsigned int);
-        glDrawElements(GL_LINE_LOOP, offset[i], GL_UNSIGNED_INT, (void *)(last_position));
-      }
-      glFlush();
-    }
-  }
-/*
-  void DrawSurfaces(s21::Matrix4f& mvp) { 
-    mvp_location_ = glGetUniformLocation(texture_shader.GetProgram(), "mvp");
-    glUseProgram(texture_shader.GetProgram());
-    glBindVertexArray(vao_);
-    glUniformMatrix4fv(mvp_location_, 1, GL_TRUE, &mvp(0,0));
-
-
-
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_->GetVertexBuffer());
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                          nullptr);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_TEXTURE_BUFFER, texture_->GetVertexBuffer());
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                          nullptr); //TODO ->
-    glEnableVertexAttribArray(1);
-
-    //glEnableVertexAttribArray(0);
-   glDepthMask(GL_TRUE);
-   glDepthFunc(GL_LEQUAL);
-   glDepthRange(0.0f, 1.0f);
-
-    //glTexBuffer(GL_TEXTURE_BUFFER, GL_TEXTURE0, texture_->GetVertexBuffer());
-
-    //glFrontFace(GL_CW);
-    //glCullFace(GL_FRONT);
-    //glEnable(GL_CULL_FACE);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture_image_.GetID());
-    //glBindTexture(GL_TEXTURE_BUFFER, texture_->GetVertexBuffer());
-    //glDrawArrays(GL_TRIANGLE_FAN, 0, s21::Figure::GetInstance()->GetVertex().size());
-   
-   // std::vector<GLuint> texture_ind = texture_->GetElementBuffer();   
-    std::vector<GLuint> element_buffer = vertex_->GetElementBuffer();
-    for(size_t i = 0; i < element_buffer.size(); ++i) {
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer[i]);
-      glDrawElements(GL_TRIANGLE_STRIP, s21::Figure::GetInstance()->GetVSurface(i).size(),
-                     GL_UNSIGNED_INT, nullptr);
-      glFlush();
-    }
-    glFlush();
-  }*/
-
-  void InitShaders() {
-    point_shader.LoadShader("glarea/point_vs.glsl", "glarea/point_fs.glsl", "glarea/point_gs.glsl");
-    line_shader.LoadShader("glarea/line_vs.glsl", "glarea/line_fs.glsl", "glarea/line_gs.glsl");
-    //texture_shader.LoadShader("glarea/texture_vs.glsl", "glarea/texture_fs.glsl", nullptr);
-  }
-
-  void InitBuffer() {
-    glGenVertexArrays(1, &vao_);
-    glBindVertexArray(vao_);
-    std::vector<float> v = s21::Figure::GetInstance()->GetVertex();
-    std::vector<unsigned int> e = s21::Figure::GetInstance()->GetVSurface();
-    vertex_->CreateBuffer(v, e);
-    /*
-    v = s21::Figure::GetInstance()->GetTextures();
-    e = s21::Figure::GetInstance()->GetTSurface();
-    texture_->CreateBuffer(v, e);
-
-    v = s21::Figure::GetInstance()->GetNormals();
-    e = s21::Figure::GetInstance()->GetNSurface();
-    normals_->CreateBuffer(v, e);*/
-  }
-
-  void ComputeMVP(Matrix4f& mvp) {
-    //Scaling(mvp);
-    //Rotation(mvp);
-    //Translation(mvp);
-    Projection(mvp);
-    Translation(mvp);
-    Rotation(mvp);
-    Scaling(mvp);
-  }
-
-  void Rotation(Matrix4f& mvp) {
-    Affine3D affine;
-    float *rotor = data_->GetRotation();
-    mvp *= affine.GetRotation(rotor[0], rotor[1], rotor[2]);
-  }
-
-  void Scaling(Matrix4f& mvp) {
-    Affine3D affine;
-    float *scale = data_->GetScaling();
-    mvp *= affine.GetScaling(scale[0], scale[1], scale[2]);
-  }
-
-  void Translation(Matrix4f& mvp) {
-    Affine3D affine;
-    float *trans = data_->GetTranslation();
-    mvp *= affine.GetTranslation(trans[0], trans[1], 5 + trans[2]);
-  }
-
-  void Projection(Matrix4f& mvp) {
-    Affine3D affine;
-   // mvp *= affine.GetTranslation(0,0,5);
-    if(data_->GetProjection() == 0) { 
-      mvp *= affine.GetParralelProjection();
-    } else {
-      mvp *= affine.GetCentralProjection();
-    }
-  }
-};
+  void ComputeMVP(Matrix4f& mvp);
+  void Rotation(Matrix4f& mvp);
+  void Scaling(Matrix4f& mvp);
+  void Translation(Matrix4f& mvp);
+  void Projection(Matrix4f& mvp);
+	};
 } // namespace s21
 
 #endif
